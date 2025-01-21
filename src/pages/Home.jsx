6da7d31df6610af0f4e54021beb3aebde7f6fbc7
KartikPandey19 <kartik2000pandey@gmail.com>
2025-01-21 18:27:13 +0530

    init

---
+++ src/pages/Home.jsx
@@ -0,0 +1,13 @@
+import Header from "../components/Header"
+import HomeCard from "../components/HomeCards"
+const Home =  ()=>{
+
+    return (
+        <>
+        <Header />
+        <HomeCard />
+        </>
+    )
+}
+
+export default Home
---
+++ src/utils/ApiEndPoints.jsx
@@ -0,0 +1,4 @@
+const apiKey = import.meta.env.CRIC_API_KEY
+export const ApiEndPoints =[
+    CurrentMatches=`https://api.cricapi.com/v1/currentMatches?apikey=${apiKey}`,
+]
